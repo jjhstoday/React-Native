40acf8d2c53900a953857fdff0a98db347773273
jjhstoday <jjh.s.today@gmail.com>
2021-06-25 17:33:49 +0900

    feat: 연락처에 접근하기

--- RNative_Contacts/App.js
+++ RNative_Contacts/App.js
@@ -7,13 +7,60 @@
  */
 
 import React, {useState} from 'react';
-import {StyleSheet, View, Text, Image, Button} from 'react-native';
+import {
+  Platform,
+  PermissionsAndroid,
+  StyleSheet,
+  View,
+  Text,
+  Button,
+} from 'react-native';
 import Contacts from 'react-native-contacts';
 
 const App = () => {
+  const requestContactPermission = async () => {
+    if (Platform.OS === 'ios') {
+      console.warn('iOS');
+      return true;
+    } else {
+      console.warn('Android');
+
+      const granted = await PermissionsAndroid.requestMultiple([
+        PermissionsAndroid.PERMISSIONS.WRITE_CONTACTS,
+        PermissionsAndroid.PERMISSIONS.READ_CONTACTS,
+      ]);
+
+      if (
+        granted['android.permission.READ_CONTACTS'] ===
+          PermissionsAndroid.RESULTS.GRANTED &&
+        granted['android.permission.WRITE_CONTACTS'] ===
+          PermissionsAndroid.RESULTS.GRANTED
+      ) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  };
+
+  const getContacts = () => {
+    requestContactPermission().then(didGetPermission => {
+      if (didGetPermission) {
+        Contacts.getAll((err, contacts) => {
+          if (err) {
+            throw err;
+          }
+          console.warn(contacts);
+        });
+      } else {
+        alert('no permission');
+      }
+    });
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Hello</Text>
+      <Button title="Load Contact" onPress={() => getContacts()} />
     </View>
   );
 };
