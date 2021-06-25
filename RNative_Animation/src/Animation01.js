6281a36294bd346a8d5ed5368203476d8f932081
jjhstoday <jjh.s.today@gmail.com>
2021-06-25 21:31:48 +0900

    feat: interpolate 효과 설정

--- RNative_Animation/src/Animation01.js
+++ RNative_Animation/src/Animation01.js
@@ -7,31 +7,59 @@
  */
 
 import React, {useState, useEffect} from 'react';
-import {Easing, Animated, StyleSheet, View, Button} from 'react-native';
+import {Easing, Animated, StyleSheet, View, Button, Text} from 'react-native';
 
 const AnimOne = () => {
-  const [mySquare, setMySquare] = useState(() => new Animated.ValueXY(0, 0));
+  const [mySquare, setMySquare] = useState(() => new Animated.Value(1));
 
   const runAnimation = () => {
     Animated.timing(mySquare, {
-      toValue: {x: 50, y: 300},
+      toValue: 0,
       duration: 2000,
       delay: 1500,
-      easing: Easing.elastic(3),
     }).start();
   };
 
   return (
     <View>
       <Animated.View
-        style={mySquare.getLayout()}
-        // style={{
-        //   left: mySquare.x,
-        //   top: mySquare.y,
-        // }}
-      >
+        // style={mySquare.getLayout()}
+        style={{
+          opacity: mySquare,
+          transform: [
+            {
+              rotateX: mySquare.interpolate({
+                inputRange: [0, 0.5, 1], // inputRange는 작은 값부터 넣어준다.
+                outputRange: ['0deg', '180deg', '360deg'], // inputRagne의 인덱스와 대응되어야 한다.
+              }),
+            },
+            {
+              translateX: mySquare.interpolate({
+                inputRange: [0, 0.5, 1],
+                outputRange: [300, 150, 0],
+              }),
+            },
+          ],
+          // top: mySquare.interpolate({
+          //   inputRange: [0, 1], // inputRange는 작은 값부터 넣어준다.
+          //   outputRange: [700, 0], // inputRagne의 인덱스와 대응되어야 한다.
+          // }),
+        }}>
         <View style={styles.square} />
       </Animated.View>
+      <Animated.Text
+        style={{
+          fontSize: mySquare.interpolate({
+            inputRange: [0, 0.5, 1],
+            outputRange: [40, 30, 20],
+          }),
+          color: mySquare.interpolate({
+            inputRange: [0, 0.5, 1],
+            outputRange: ['red', 'green', 'blue'],
+          }),
+        }}>
+        <Text>Animation Effects</Text>
+      </Animated.Text>
       <Button title="Animation Start" onPress={runAnimation} />
     </View>
   );
